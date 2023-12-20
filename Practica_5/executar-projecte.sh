757abbda0703d5e7acd8db9e1ab710e28883cd74
MCrumo <miquel1rr@gmail.com>
2023-12-20 22:20:09 +0100

    Afegint configuracio

--- Practica_5/executar-projecte.sh
+++ Practica_5/executar-projecte.sh
@@ -2,9 +2,9 @@
 
 cd ./ClientAD/ClientAD
 mvn clean install
-sudo cp target/ClientAD-1.0.war /opt/tomcat/webapps/
+#sudo cp target/ClientAD-1.0.war /opt/tomcat/webapps/
 
 cd ../../
-docekr-compose up -d --build
-sleep 2
-google-chrome --incognito http://localhost:8080/ClientAD-1.0
+docker-compose up --build
+#sleep 2
+#google-chrome --incognito http://localhost:8080/ClientAD-1.0
