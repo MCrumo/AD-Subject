7f2bec9e9295c93ff8e3c4948cbb2c1671ea0901
MCrumo <miquel1rr@gmail.com>
2023-12-20 13:50:49 +0100

    Afegit dockerfiles

--- Practica_5/ClientAD/run-client.sh
+++ Practica_5/ClientAD/run-client.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+#systemctl start tomcat.service
+#cd ./ClientAD
+#mvn clean install
+#sudo cp target/ClientAD-1.0.war /opt/tomcat/webapps/
+#sudo systemctl restart tomcat
+#sudo journalctl -u tomcat -xe
 
-systemctl start tomcat.service
-
-cd ./ClientAD
-
-mvn clean install
-
-sudo cp target/ClientAD-1.0.war /opt/tomcat/webapps/
-
-sudo systemctl restart tomcat
-
-sudo journalctl -u tomcat -xe
+docker build -t tomcat-image .
+docker run -p 8080:8080 -d tomcat-image
+sleep 1
+google-chrome --incognito http://localhost:8080/ClientAD-1.0
